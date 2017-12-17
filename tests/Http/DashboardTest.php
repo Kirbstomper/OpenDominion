cf56e53bd8b02c8a50fb457b4e4b96378eb7d466
Kirbstomper <WingedKraby@gmail.com>
2017-12-17 14:56:35 -0500

    Refactored DashboardTest

--- tests/Http/DashboardTest.php
+++ tests/Http/DashboardTest.php
@@ -4,19 +4,20 @@ namespace OpenDominion\Tests\Http;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use OpenDominion\Tests\AbstractBrowserKitTestCase;
+use OpenDominion\Tests\AbstractTestCase;
 
-class DashboardTest extends AbstractBrowserKitTestCase
+class DashboardTest extends AbstractTestCase
 {
     use DatabaseMigrations;
 
     public function testDashboardPage()
     {
-        $this->visitRoute('dashboard')
-            ->seeRouteIs('auth.login');
+        $response = $this->get('/dashboard');
+        $response->assertRedirect('/auth/login');
 
         $this->createAndImpersonateUser();
 
-        $this->visitRoute('dashboard')
-            ->seeStatusCode(200);
+        $response = $this->get('/dashboard');
+        $response->assertStatus(200);
     }
 }
